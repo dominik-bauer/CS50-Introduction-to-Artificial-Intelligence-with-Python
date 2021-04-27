4ee503d1735a3f0d5701a546046e55f4ef3bfa4d
dominik-bauer <dominik_b@gmx.de>
2021-04-27 15:11:31 +0200

    First working Knights and Knaves puzzle...

--- knights/puzzle.py
+++ knights/puzzle.py
@@ -12,21 +12,42 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    Or(AKnave, AKnight),                             # A can only be one: a Knight or a Knave
+    Implication(AKnave, Not(And(AKnave, AKnight))),  # Whatever a Knave says is wrong --> NOT
+    Implication(AKnight,    And(AKnave, AKnight))    # Whatever a Knight says is correct
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    Or(AKnave, AKnight),  # A can only be a Knight or a Knave
+    Or(BKnave, BKnight),  # B can only be a Knight or a Knave
+    Implication(AKnave, Not(And(AKnave, BKnave))),  # Only B says something, here as a Knave
+    Implication(AKnight,    And(AKnave, BKnave))    # Only B says something, here as a Knight
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
+
+# helper sentence
+being_same = Or(And(AKnave,  BKnave),
+                And(AKnight, BKnight))
+being_different = Or(And(AKnave, BKnight),
+                     And(AKnight, BKnave))
+
 knowledge2 = And(
-    # TODO
+    Or(AKnave, AKnight),  # A can only be a Knight or a Knave
+    Or(BKnave, BKnight),  # B can only be a Knight or a Knave
+
+    # A expressing that both are of the same kind
+    Implication(AKnave, Not(being_same)),
+    Implication(AKnight,    being_same),
+
+    # B expressing that both are of different kind
+    Implication(BKnave,   Not(being_different)),
+    Implication(BKnight,     being_different)
 )
 
 # Puzzle 3
@@ -34,8 +55,27 @@ knowledge2 = And(
 # B says "A said 'I am a knave'."
 # B says "C is a knave."
 # C says "A is a knight."
+
+# Helper sentence
+A2 = And(Implication(AKnave, Not(AKnave)),
+         Implication(AKnight,    AKnave))
+
 knowledge3 = And(
-    # TODO
+    Or(AKnave, AKnight),  # A can only be a Knight or a Knave
+    Or(BKnave, BKnight),  # B can only be a Knight or a Knave
+    Or(CKnave, CKnight),  # C can only be a Knight or a Knave
+
+    # B says some about A, as Knave and as a Knight, ...
+    Implication(BKnave, Not(A2)),
+    Implication(BKnight,    A2),
+
+    # B also says ,as Knave and Knight, ...
+    Implication(BKnave, Not(CKnave)),
+    Implication(BKnight,    CKnave),
+
+    # C speaks as Knave and Knight:
+    Implication(CKnave, Not(AKnight)),
+    Implication(CKnight,    AKnight)
 )
 
 
